3374a3a9cefdaf144eabdbf9499331a8790ff849
Eric Shapiro <eshapiro42@gmail.com>
2022-04-05 02:51:18 -0700

    Revert "Add keyboard shortcuts for Select All and Send Selection"
    This reverts commit 8097307ec57f3aa0b996068d61a4c6291e9fd42a.

--- client/src/components/selection_prompt.svelte
+++ client/src/components/selection_prompt.svelte
@@ -17,41 +17,15 @@
         );
     }
 
-    function handleEnterKey(event) {
-        if (event.code == "Enter" && waitingForSelection) {
-            handleSendSelection();
-        }
-    }
-
-    function handleAKey(event) {
-        if (event.code == "KeyA" && waitingForSelection) {
-            handleSelectAll();
-        }
-    }
-
-    function ignoreKeyPresses(event) {
-        event.preventDefault();
-    }
-
     $: renderedPrompt = waitingForSelection.prompt.replace("$", `<i class="fa-solid fa-coins"></i>`);
 
-    $: selectAllEnabled = waitingForSelection.maxCards == null && waitingForSelection.maxCards != 1;
-
-    $: if (waitingForSelection.value) {
-        document.addEventListener("keyup", handleEnterKey, {once: true});
-        if (selectAllEnabled) {
-            document.addEventListener("keyup", handleAKey, {once: true});
-        } else {
-            document.addEventListener("keyup", ignoreKeyPresses);
-        }
-    }
 </script>
 
 <main>
     <p class="flex-item"><b>{@html renderedPrompt}</b></p>
     <div class="flex-item">
         <button type="button" on:click={handleSendSelection}>Send Selection</button>
-        {#if selectAllEnabled}
+        {#if waitingForSelection.maxCards == null && waitingForSelection.maxCards != 1}
             <button type="button" on:click={handleSelectAll}>Select All</button>
         {/if}
     </div>
