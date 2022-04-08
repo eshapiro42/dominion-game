d7e3f934081785001e1e685f0052332800e55abc
Eric Shapiro <eshapiro42@gmail.com>
2022-04-08 16:06:46 -0700

    Add tooltips for customization options
    Fixes #53

--- client/src/components/game_setup.svelte
+++ client/src/components/game_setup.svelte
@@ -21,13 +21,65 @@
     ]
 
     var supplyCustomizations = [
-        {name: "Allow simultaneous reactions", property: "allowSimultaneousReactions", selected: false},
-        {name: "Distribute cost", property: "distributeCost", selected: false},
-        {name: "Disable Attacks", property: "disableAttacks", selected: false},
-        {name: "Require +2 Action", property: "requirePlusTwoAction", selected: false},
-        {name: "Require +1 Card", property: "requireDrawer", selected: false},
-        {name: "Require +1 Buy", property: "requireBuy", selected: false},
-        {name: "Require Trashing", property: "requireTrashing", selected: false},
+        {
+            name: "Allow simultaneous reactions",
+            property: "allowSimultaneousReactions",
+            selected: false,
+            description: [
+                "Allow attacked players to react to an Attack simultaneously and asynchronously (when doing so is sensible).",
+                "This currently only works for Attack effects where the only required input is from the players being attacked, such as with the Militia. If the Attack requires feedback from the attacker, like with the Spy, Thief, Swindler, etc, the frontend is not currently equipped to deal with simultaneous reactions.",
+                "This also cannot be used for Attacks that require the attacked players to gain cards from the Supply, such as the Witch, Swindler, Torturer, Replace, etc. That's because there may be only one of a given card left in the Supply, and therefore the effect must be resolved in turn order."
+            ],
+        },
+        {
+            name: "Distribute cost",
+            property: "distributeCost", 
+            selected: false,
+            description: [
+                "Ensure that there are at least two cards each in the Supply of cost 2, 3, 4 and 5.",
+            ],
+        },
+        {
+            name: "Disable Attacks",
+            property: "disableAttacks", 
+            selected: false,
+            description: [
+                "Do not allow attack cards in the Supply.",
+            ],
+        },
+        {
+            name: "Require +2 Action",
+            property: "requirePlusTwoAction", 
+            selected: false,
+            description: [
+                "Require that at least one card in the Supply gives +2 Actions or more.",
+            ],
+        },
+        {
+            name: "Require +1 Card",
+            property: "requireDrawer", 
+            selected: false,
+            description: [
+                "Require that at least one card in the Supply gives +1 Card or more.",
+            ],
+        },
+        {
+            name: "Require +1 Buy",
+            property: "requireBuy", 
+            selected: false,
+            description: [
+                "Require that at least one card in the Supply gives +1 Buy or more.",
+            ],
+        },
+        {
+            name: "Require Trashing",
+            property: "requireTrashing", 
+            selected: false,
+            description: [
+                "Require that at least one card in the Supply allows trashing.",
+                "This is currently implemented by searching for cards with the word 'trash' in their description. This may not be a totally accurate method.",
+            ],
+        },
     ]
 
     function addCPU() {
@@ -76,28 +128,39 @@
 {#if shown}
     {#if roomJoined && roomCreator}
         <main>
-            <button type="button" on:click={startGame} class="btn btn-primary btn-lg btn-block">Start Game</button>
-            <button type="button" on:click={addCPU} class="btn btn-secondary btn-lg btn-block">Add CPU</button>
-            <div class="grid-item">
+            <div class="buttons">
+                <button type="button" on:click={startGame} class="btn btn-primary btn-lg btn-block">Start Game</button>
+                <button type="button" on:click={addCPU} class="btn btn-secondary btn-lg btn-block">Add CPU</button>
+            </div>
+            <div class="customizations">
                 {#each expansions as expansion}
-                    <label>
+                    <label class="customization">
                         <input type="checkbox" bind:checked={expansion.selected}>
                         {expansion.name} Expansion
                     </label>
                 {/each}
             </div>
-            <div class="grid-item">
+            <div class="customizations">
                 {#each supplyCustomizations as customization}
-                    <label>
+                    <label class="customization">
                         <input type="checkbox" bind:checked={customization.selected}>
-                        {customization.name}
+                        <div class="hoverable">
+                            {customization.name}
+                            <span class="hoverable-text">
+                                {#each customization.description as line}
+                                    <span class="hoverable-text-line">
+                                        {line}
+                                    </span>
+                                {/each}
+                            </span>
+                        </div>
                     </label>
                 {/each}
             </div>
         </main>
     {:else if roomJoined}
         <main>
-            <div class="grid-item-2">
+            <div>
                 <p>Please wait for the host to start the game.</p>
             </div>
         </main>
@@ -107,20 +170,50 @@
 <style>
     main {
         margin-top: 50px;
-        display: grid;
-        grid-template-columns: repeat(2, 50%);
-        justify-items: center;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
     }
 
-    .grid-item {
-        grid-column-start: auto;
-        justify-self: center;
+    .buttons {
+        flex-basis: 100%;
+        margin-bottom: 50px;
+    }
+
+    .customization {
+        padding: 10px;
+    }
+
+    .customizations {
         text-align: left;
     }
 
-    .grid-item-2 {
-        grid-column: 1/-1;
-        justify-self: center;
-        text-align: center;
+    .hoverable {
+        position: relative;
+        display: inline-block;
+        border-bottom: 1px dotted #343338;
+    }
+
+    .hoverable .hoverable-text {
+        visibility: hidden;
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 10px;
+        z-index: 1;
+        font-size: 85%;
+        width: 300px;
+        background-color: #343338;
+        color: #dadada;
+        text-align: left;
+        overflow-wrap: break-word;
+        border-radius: 10px;
+        padding: 10px;
+    }
+
+    .hoverable:hover .hoverable-text {
+        visibility: visible;
     }
 </style>
