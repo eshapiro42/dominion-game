35a4b0cd26b6ec436ba0510109d6455da9419ea2
Eric Shapiro <eshapiro42@gmail.com>
2023-12-21 19:41:20 -0500

    Remove hashes from URL bar

--- client/src/components/card_carousel.svelte
+++ client/src/components/card_carousel.svelte
@@ -119,6 +119,7 @@
         setTimeout(
             () => {
                 location.hash = "#" + title;
+                history.pushState("", document.title, window.location.pathname + window.location.search);
             },
             300,
         );
